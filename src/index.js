e0e36fd56ad7c830da23de628a8758c78af4cd3f
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-11-02 04:41:42 +0200

    - add reducer import to index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import  {createStore} from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from "./Store/persons_reducer";
 
 const store = createStore(reducer);
 
